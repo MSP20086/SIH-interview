639c78d121cb47d1004c9f5b44d57925282af8bd
Tanay Gada <tanaygada14@gmail.com>
2024-08-29 00:16:13 +0530

    dashboard created

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -27,7 +27,6 @@ export default function RootLayout({ children }) {
         <NextUIProvider>
           <Header />
           {children}
-          <Banner />
         </NextUIProvider>
         </div>
       </body>
