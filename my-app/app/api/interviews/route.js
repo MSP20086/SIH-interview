53cc0b34c155fe590e9a5ea09dc1ecf82209cc54
MSP20086 <mihirpandit04@gmail.com>
2024-08-29 23:25:30 +0530

    a

--- my-app/app/api/interviews/route.js
+++ my-app/app/api/interviews/route.js
@@ -1,12 +1,12 @@
-import { connectToDatabase } from '@/lib/mongodb'; 
+import { connectToDatabase } from '@/lib/mongodb';
 
 export async function POST(req) {
     try {
         const { name, email, jobPosition, interviewTime, HostLink, candidateLink } = await req.json();
 
-        // if (!name || !email || !jobPosition || !interviewTime || !HostLink || !candidateLink) {
-        //     return new Response(JSON.stringify({ message: 'All fields are required' }), { status: 400 });
-        // }
+        if (!name || !email || !jobPosition || !interviewTime || !HostLink || !candidateLink) {
+            return new Response(JSON.stringify({ message: 'All fields are required' }), { status: 400 });
+        }
 
         const { db } = await connectToDatabase();
 
