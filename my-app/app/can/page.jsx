403f1080a45ced7a74cd190521db0dafa026db91
JJB825 <jish2homeworld@gmail.com>
2024-09-04 09:21:58 +0530

    added feedback form

--- my-app/app/can/page.jsx
+++ my-app/app/can/page.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import { Button, Link } from '@nextui-org/react'
 import { useUser } from '@/app/context/user'
+import FeedbackForm from '@/components/feedback-form/page'
 
 export default function Page() {
   const searchParams = useSearchParams()
@@ -12,6 +13,7 @@ export default function Page() {
 
   const [meetlink, setMeetLink] = useState('')
   const [showmeetlink, setShowMeetLink] = useState(false)
+  const [showFeedbackPage, setShowFeedbackPage] = useState(false)
   const [formData, setFormData] = useState({
     id: '',
     name: '',
@@ -47,10 +49,10 @@ export default function Page() {
 
   if (loading) {
     return (
-      <section className="bg-gradient-to-b from-gray-100 to-white h-screen flex items-center justify-center">
-        <div className="flex items-center">
-          <div className="w-8 h-8 border-4 border-blue-600 border-t-transparent border-solid rounded-full animate-spin"></div>
-          <p className="ml-4 text-gray-800">Loading user data...</p>
+      <section className='bg-gradient-to-b from-gray-100 to-white h-screen flex items-center justify-center'>
+        <div className='flex items-center'>
+          <div className='w-8 h-8 border-4 border-blue-600 border-t-transparent border-solid rounded-full animate-spin'></div>
+          <p className='ml-4 text-gray-800'>Loading user data...</p>
         </div>
       </section>
     )
@@ -58,8 +60,10 @@ export default function Page() {
 
   if (!isUser) {
     return (
-      <section className="bg-gradient-to-b from-gray-100 to-white h-screen flex items-center justify-center">
-        <p className="text-gray-800">Access denied. You must be a candidate to view this page.</p>
+      <section className='bg-gradient-to-b from-gray-100 to-white h-screen flex items-center justify-center'>
+        <p className='text-gray-800'>
+          Access denied. You must be a candidate to view this page.
+        </p>
       </section>
     )
   }
@@ -114,126 +118,175 @@ export default function Page() {
     }
   }
 
+  if (showFeedbackPage) return <FeedbackForm />
+
   return (
-    <div className="flex flex-col md:flex-row min-h-screen md:h-screen pt-16 md:pt-20 bg-gray-50 overflow-y-auto">
+    <div className='flex flex-col md:flex-row min-h-screen md:h-screen pt-16 md:pt-20 bg-gray-50 overflow-y-auto'>
       {/* Left side with a welcome message and decorative elements */}
-      <div className="md:w-1/2 p-8 md:p-16 bg-indigo-600 text-white shadow-lg rounded-lg md:rounded-none md:rounded-l-lg flex flex-col justify-center items-center">
-        <div className="text-center">
-          <h1 className="text-4xl font-extrabold mb-4">Interview Portal</h1>
-          <p className="text-lg mb-6">Join us to take the next step in your career!</p>
-          <div className="bg-white text-indigo-600 font-bold rounded-full px-6 py-2 inline-block">
+      <div className='md:w-1/2 p-8 md:p-16 bg-indigo-600 text-white shadow-lg rounded-lg md:rounded-none md:rounded-l-lg flex flex-col justify-center items-center'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-extrabold mb-4'>Interview Portal</h1>
+          <p className='text-lg mb-6'>
+            Join us to take the next step in your career!
+          </p>
+          <div className='bg-white text-indigo-600 font-bold rounded-full px-6 py-2 inline-block'>
             <p>FAQ</p>
           </div>
         </div>
       </div>
 
       {/* Right side with instructions and the form */}
-      <div className="md:w-1/2 p-10 md:p-20 bg-white shadow-lg rounded-lg md:rounded-none md:rounded-r-lg overflow-y-auto">
-        <h2 className="text-3xl font-bold mb-6 text-gray-800">Welcome to Interview!</h2>
-        <p className="text-lg mb-6 text-gray-600">Please fill in the form below with your details to proceed:</p>
-        <ul className="list-disc list-inside text-gray-700 mb-8">
-          <li className="mb-2">Enter your full name</li>
-          <li className="mb-2">Provide a valid email address</li>
-          <li className="mb-2">List your relevant skill sets</li>
-          <li className="mb-2">Upload your latest resume in PDF format</li>
-          <li className="mb-2">Before taking the interview, please go through the FAQs to resolve your queries related to the interview or the platform.</li>
+      <div className='md:w-1/2 p-10 md:p-20 bg-white shadow-lg rounded-lg md:rounded-none md:rounded-r-lg overflow-y-auto'>
+        <h2 className='text-3xl font-bold mb-6 text-gray-800'>
+          Welcome to Interview!
+        </h2>
+        <p className='text-lg mb-6 text-gray-600'>
+          Please fill in the form below with your details to proceed:
+        </p>
+        <ul className='list-disc list-inside text-gray-700 mb-8'>
+          <li className='mb-2'>Enter your full name</li>
+          <li className='mb-2'>Provide a valid email address</li>
+          <li className='mb-2'>List your relevant skill sets</li>
+          <li className='mb-2'>Upload your latest resume in PDF format</li>
+          <li className='mb-2'>
+            Before taking the interview, please go through the FAQs to resolve
+            your queries related to the interview or the platform.
+          </li>
         </ul>
 
         {/* Form */}
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <form onSubmit={handleSubmit} className='space-y-4'>
           <div>
-            <label htmlFor="name" className="block text-gray-700">Full Name</label>
+            <label htmlFor='name' className='block text-gray-700'>
+              Full Name
+            </label>
             <input
-              type="text"
-              id="name"
-              name="name"
+              type='text'
+              id='name'
+              name='name'
               value={formData.name}
               onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded-lg"
+              className='w-full p-2 border border-gray-300 rounded-lg'
               required
             />
           </div>
 
           <div>
-            <label htmlFor="email" className="block text-gray-700">Email Address</label>
+            <label htmlFor='email' className='block text-gray-700'>
+              Email Address
+            </label>
             <input
-              type="email"
-              id="email"
-              name="email"
+              type='email'
+              id='email'
+              name='email'
               value={formData.email}
               onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded-lg"
+              className='w-full p-2 border border-gray-300 rounded-lg'
               required
             />
           </div>
           <div>
-            <label htmlFor="skillSets" className="block text-gray-700">Skill Sets</label>
+            <label htmlFor='skillSets' className='block text-gray-700'>
+              Skill Sets
+            </label>
             <textarea
-              id="skillSets"
-              name="skillSets"
+              id='skillSets'
+              name='skillSets'
               value={formData.skillSets}
               onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded-lg"
+              className='w-full p-2 border border-gray-300 rounded-lg'
               required
             />
           </div>
 
           <div>
-            <label htmlFor="resume" className="block text-gray-700">Upload Resume</label>
+            <label htmlFor='resume' className='block text-gray-700'>
+              Upload Resume
+            </label>
             <input
-              type="file"
-              id="resume"
-              name="resume"
-              accept=".pdf"
+              type='file'
+              id='resume'
+              name='resume'
+              accept='.pdf'
               onChange={handleFileChange}
-              className="w-full p-2 border border-gray-300 rounded-lg"
+              className='w-full p-2 border border-gray-300 rounded-lg'
               required
             />
           </div>
 
           <div>
             <button
-              type="submit"
+              type='submit'
               className={`w-full p-2 rounded-lg font-semibold transition-all duration-300 
-                ${isSubmitting
-                  ? 'bg-gray-400 text-gray-700 cursor-not-allowed'
-                  : 'bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+                ${
+                  isSubmitting
+                    ? 'bg-gray-400 text-gray-700 cursor-not-allowed'
+                    : 'bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500'
                 }`}
               disabled={isSubmitting}
             >
-              {isSubmitting
-                ? (
-                  <span className="flex items-center justify-center">
-                    <svg className="w-5 h-5 mr-2 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 1116 0A8 8 0 014 12z"></path>
-                    </svg>
-                    Submitting...
-                  </span>
-                )
-                : 'Submit'
-              }
+              {isSubmitting ? (
+                <span className='flex items-center justify-center'>
+                  <svg
+                    className='w-5 h-5 mr-2 animate-spin text-white'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                  >
+                    <circle
+                      className='opacity-25'
+                      cx='12'
+                      cy='12'
+                      r='10'
+                      stroke='currentColor'
+                      strokeWidth='4'
+                    ></circle>
+                    <path
+                      className='opacity-75'
+                      fill='currentColor'
+                      d='M4 12a8 8 0 1116 0A8 8 0 014 12z'
+                    ></path>
+                  </svg>
+                  Submitting...
+                </span>
+              ) : (
+                'Submit'
+              )}
             </button>
           </div>
 
           {responseMessage && (
-            <div className="text-center mt-4">
-              <p className={`text-lg ${responseMessage.includes('error') ? 'text-red-600' : 'text-green-600'}`}>
+            <div className='text-center mt-4'>
+              <p
+                className={`text-lg ${
+                  responseMessage.includes('error')
+                    ? 'text-red-600'
+                    : 'text-green-600'
+                }`}
+              >
                 {responseMessage}
               </p>
             </div>
           )}
 
-          <div className="text-sm text-gray-500 text-center mt-3">
-            By submitting, you agree to the <a className="underline" href="#0">terms & conditions</a>, and our <a className="underline" href="#0">privacy policy</a>.
+          <div className='text-sm text-gray-500 text-center mt-3'>
+            By submitting, you agree to the{' '}
+            <a className='underline' href='#0'>
+              terms & conditions
+            </a>
+            , and our{' '}
+            <a className='underline' href='#0'>
+              privacy policy
+            </a>
+            .
           </div>
         </form>
         {showmeetlink && (
           <Button
             as={Link}
             href={meetlink}
-            target="_blank"
-            className="w-full mt-4 p-2 rounded-lg font-semibold transition-all duration-300 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            onClick={() => setShowFeedbackPage(true)}
+            target='_blank'
+            className='w-full mt-4 p-2 rounded-lg font-semibold transition-all duration-300 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500'
           >
             Join the Interview
           </Button>
---
+++ my-app/components/feedback-form/page.jsx
@@ -0,0 +1,291 @@
+'use client'
+
+import { useState } from 'react'
+import { Textarea } from '@nextui-org/react'
+
+export default function FeedbackForm() {
+  const [rating, setRating] = useState(0)
+  const [questionRelevance, setQuestionRelevance] = useState(0)
+  const [difficultyLevel, setDifficultyLevel] = useState(0)
+  const [questionClarity, setQuestionClarity] = useState(0)
+  const [expertFeedback, setExpertFeedback] = useState(0)
+  const [recommendation, setRecommendation] = useState(0)
+  const [questionsFeedback, setQuestionsFeedback] = useState('')
+  const [improvements, setImprovements] = useState('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Handle form submission here
+    setIsSubmitting(true)
+
+    const feedbackData = []
+    feedbackData.push({ rating: rating })
+    feedbackData.push({ questionRelevance: questionRelevance })
+    feedbackData.push({ difficultyLevel: difficultyLevel })
+    feedbackData.push({ questionClarity: questionClarity })
+    feedbackData.push({ expertFeedback: expertFeedback })
+    feedbackData.push({ recommendation: recommendation })
+    feedbackData.push({ questionsFeedback: questionsFeedback })
+    feedbackData.push({ improvements: improvements })
+
+    // console logging for now
+    console.log(feedbackData)
+    setIsSubmitting(false)
+  }
+
+  return (
+    <div className='flex flex-col md:flex-row min-h-screen md:h-screen pt-16 md:pt-20 bg-gray-50 overflow-y-auto'>
+      {/* Left side with a welcome message and decorative elements */}
+      <div className='md:w-1/2 p-8 md:p-16 bg-indigo-600 text-white shadow-lg rounded-lg md:rounded-none md:rounded-l-lg flex flex-col justify-center items-center'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-extrabold mb-4'>Interview Portal</h1>
+          <p className='text-lg mb-6'>
+            Join us to take the next step in your career!
+          </p>
+          <div className='bg-white text-indigo-600 font-bold rounded-full px-6 py-2 inline-block'>
+            <p>FAQ</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Right side with feedback form */}
+      <div className='md:w-1/2 p-10 md:p-20 bg-white shadow-lg rounded-lg md:rounded-none md:rounded-r-lg overflow-y-auto'>
+        <h2 className='text-3xl font-bold mb-6 text-gray-800'>
+          We would love to hear your thoughts, suggestions, concerns or problems
+          with anything so we can improve!
+        </h2>
+
+        {/* Form */}
+        <form onSubmit={handleSubmit} className='space-y-4'>
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='overall-experience'>
+              How would you rate your overall experience with the interview
+              process? <span className='text-red-500'>*</span>
+            </label>
+            <div
+              id='overall-experience'
+              style={{ display: 'flex', gap: '10px', marginTop: '10px' }}
+              required
+            >
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setRating(index + 1)}
+                  style={{
+                    cursor: 'pointer',
+                    color: rating > index ? '#ffd700' : '#ccc',
+                    fontSize: '30px',
+                  }}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='question-relevance'>
+              How relevant were the questions to your skillset and experience as
+              mentioned in your resume? <span className='text-red-500'>*</span>
+            </label>
+            <div
+              id='question-relevance'
+              style={{ display: 'flex', gap: '10px', marginTop: '10px' }}
+              required
+            >
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setQuestionRelevance(index + 1)}
+                  style={{
+                    cursor: 'pointer',
+                    color: questionRelevance > index ? '#ffd700' : '#ccc',
+                    fontSize: '30px',
+                  }}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='difficulty-level'>
+              How would you rate the difficulty level of the questions?{' '}
+              <span className='text-red-500'>*</span>
+            </label>
+            <div
+              id='difficulty-level'
+              style={{ display: 'flex', gap: '10px', marginTop: '10px' }}
+              required
+            >
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setDifficultyLevel(index + 1)}
+                  style={{
+                    cursor: 'pointer',
+                    color: difficultyLevel > index ? '#ffd700' : '#ccc',
+                    fontSize: '30px',
+                  }}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='question-clarity'>
+              How clear and understandable were the questions posed during the
+              interview? <span className='text-red-500'>*</span>
+            </label>
+            <div
+              id='question-clarity'
+              style={{ display: 'flex', gap: '10px', marginTop: '10px' }}
+              required
+            >
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setQuestionClarity(index + 1)}
+                  style={{
+                    cursor: 'pointer',
+                    color: questionClarity > index ? '#ffd700' : '#ccc',
+                    fontSize: '30px',
+                  }}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label
+              htmlFor='questions-feedback'
+              style={{ marginBottom: '10px' }}
+            >
+              Please provide specific feedback on any question(s) that you felt
+              were not relevant to your skills.{' '}
+              <span className='text-red-500'>*</span>
+            </label>
+            <Textarea
+              placeholder='Enter your feedback'
+              onChange={(e) => setQuestionsFeedback(e.target.value)}
+              className='max-w-xs'
+              required
+            />
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='expert-feedback'>
+              How would you rate your interaction with the expert?{' '}
+              <span className='text-red-500'>*</span>
+            </label>
+            <div
+              id='expert-feedback'
+              style={{ display: 'flex', gap: '10px', marginTop: '10px' }}
+              required
+            >
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setExpertFeedback(index + 1)}
+                  style={{
+                    cursor: 'pointer',
+                    color: expertFeedback > index ? '#ffd700' : '#ccc',
+                    fontSize: '30px',
+                  }}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='improvements' style={{ marginBottom: '10px' }}>
+              Do you have any suggestions for improving the interview process or
+              the questions generated? <span className='text-red-500'>*</span>
+            </label>
+            <Textarea
+              placeholder='Enter your feedback'
+              onChange={(e) => setImprovements(e.target.value)}
+              className='max-w-xs'
+              required
+            />
+          </div>
+
+          <div style={{ marginBottom: '20px' }}>
+            <label htmlFor='recommendation'>
+              How likely are you to recommend this interview simulator to others
+              applying for similar roles?{' '}
+              <span className='text-red-500'>*</span>
+            </label>
+            <div
+              id='recommendation'
+              style={{ display: 'flex', gap: '10px', marginTop: '10px' }}
+              required
+            >
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setRecommendation(index + 1)}
+                  style={{
+                    cursor: 'pointer',
+                    color: recommendation > index ? '#ffd700' : '#ccc',
+                    fontSize: '30px',
+                  }}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <button
+              type='submit'
+              className={`w-full p-2 rounded-lg font-semibold transition-all duration-300 
+                ${
+                  isSubmitting
+                    ? 'bg-gray-400 text-gray-700 cursor-not-allowed'
+                    : 'bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+                }`}
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <span className='flex items-center justify-center'>
+                  <svg
+                    className='w-5 h-5 mr-2 animate-spin text-white'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                  >
+                    <circle
+                      className='opacity-25'
+                      cx='12'
+                      cy='12'
+                      r='10'
+                      stroke='currentColor'
+                      strokeWidth='4'
+                    ></circle>
+                    <path
+                      className='opacity-75'
+                      fill='currentColor'
+                      d='M4 12a8 8 0 1116 0A8 8 0 014 12z'
+                    ></path>
+                  </svg>
+                  Submitting...
+                </span>
+              ) : (
+                'Submit'
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
